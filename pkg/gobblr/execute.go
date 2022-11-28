3743c339969342d0a770cf6abe576ebc58a39934
Simon Emms <simon@simonemms.com>
2022-11-28 16:46:15 +0000

    feat(drivers): create the interface for the driver and how it's used

---
+++ pkg/gobblr/execute.go
@@ -0,0 +1,9 @@
+package gobblr
+
+import "github.com/mrsimonemms/gobblr/pkg/drivers"
+
+func Execute(dataPath string, driver drivers.Driver) (map[string]int, error) {
+	inserted := make(map[string]int, 0)
+
+	return inserted, nil
+}
