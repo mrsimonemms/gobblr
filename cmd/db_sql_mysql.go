bec397de95f99e42d79b9ebbce375e8974a464d5
Simon Emms <simon@simonemms.com>
2022-11-28 16:50:09 +0000

    feat(sql): create a mysql driver using gorm

--- cmd/db_sql_mysql.go
+++ cmd/db_sql_mysql.go
@@ -16,15 +16,43 @@ limitations under the License.
 package cmd
 
 import (
+	"github.com/mrsimonemms/gobblr/pkg/drivers/sql"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var dbSqlMysqlOpts struct {
+	Database string
+	Host     string
+	Password string
+	Port     int
+	User     string
+}
 
 // dbSqlMysqlCmd represents the mysql command
 var dbSqlMysqlCmd = &cobra.Command{
 	Use:   "mysql",
 	Short: "MySQL ingestion commands",
+	Run: func(cmd *cobra.Command, args []string) {
+		dbOpts.Driver = sql.MySQL(
+			dbSqlMysqlOpts.Database,
+			dbSqlMysqlOpts.Host,
+			dbSqlMysqlOpts.Password,
+			dbSqlMysqlOpts.Port,
+			dbSqlMysqlOpts.User,
+		)
+	},
 }
 
 func init() {
 	dbSqlCmd.AddCommand(dbSqlMysqlCmd)
+
+	viper.SetDefault("host", "localhost")
+	viper.SetDefault("port", 3306)
+	viper.SetDefault("username", "root")
+	dbSqlMysqlCmd.Flags().StringVarP(&dbSqlMysqlOpts.Database, "database", "d", viper.GetString("database"), "name of the database to use")
+	dbSqlMysqlCmd.Flags().StringVarP(&dbSqlMysqlOpts.Host, "host", "H", viper.GetString("host"), "database host name")
+	dbSqlMysqlCmd.Flags().StringVarP(&dbSqlMysqlOpts.Password, "password", "p", viper.GetString("password"), "database password")
+	dbSqlMysqlCmd.Flags().IntVarP(&dbSqlMysqlOpts.Port, "port", "P", viper.GetInt("port"), "database port")
+	dbSqlMysqlCmd.Flags().StringVarP(&dbSqlMysqlOpts.User, "username", "u", viper.GetString("username"), "database username")
 }
