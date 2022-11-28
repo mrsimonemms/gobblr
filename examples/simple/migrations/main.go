9cb85fb0755c39b972966e31195a93e9e9d1579d
Simon Emms <simon@simonemms.com>
2022-11-28 21:47:44 +0000

    chore(data): add example simple migration

---
+++ examples/simple/migrations/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Registered   bool
+	Confirmed    bool
+	Username     string
+	EmailAddress string
+	Name         string
+	Password     string
+}
+
+func main() {
+	connection := os.Getenv("DB_CONNECTION")
+	dbType := os.Getenv("DB_TYPE")
+
+	var dialector gorm.Dialector
+	switch dbType {
+	case "mysql":
+		dialector = mysql.Open(connection)
+	default:
+		panic(fmt.Errorf("unknown db type: %s", dbType))
+	}
+
+	db, err := gorm.Open(dialector, &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	if err := db.AutoMigrate(&User{}); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Migration completed")
+}
