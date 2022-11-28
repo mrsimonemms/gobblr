b47557f421f2a04b384ef7f8af2380951c0b4051
Simon Emms <simon@simonemms.com>
2022-11-28 21:48:10 +0000

    feat(gobblr): find files in gobblr format

--- pkg/gobblr/execute.go
+++ pkg/gobblr/execute.go
@@ -1,9 +1,41 @@
 package gobblr
 
-import "github.com/mrsimonemms/gobblr/pkg/drivers"
+import (
+	"fmt"
 
-func Execute(dataPath string, driver drivers.Driver) (map[string]int, error) {
+	"github.com/mrsimonemms/gobblr/pkg/drivers"
+)
+
+func Execute(dataPath string, db drivers.Driver) ( map[string]int,  error) {
 	inserted := make(map[string]int, 0)
+
+	// Connect to database
+	if err := db.Auth(); err != nil {
+		return nil, err
+	}
+
+	// Find the files
+	files, err := FindFiles(dataPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// Iterate over each file, delete and then ingest data
+	for _, file := range files {
+		fileData, err := file.LoadFile()
+		if err != nil {
+			return nil, err
+		}
+
+		if len(fileData.Data) == 0 {
+			return inserted, fmt.Errorf("no data in file: %s", file.Path)
+		}
+	}
+
+	// We've finished - let's clear up after ourselves
+	if err := db.Close(); err != nil {
+		return nil, err
+	}
 
 	return inserted, nil
 }
