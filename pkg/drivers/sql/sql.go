c3d861325c3f603514d871d7447cd4de958b33a8
Simon Emms <simon@simonemms.com>
2022-11-28 16:50:09 +0000

    feat(sql): create a mysql driver using gorm

---
+++ pkg/drivers/sql/sql.go
@@ -0,0 +1,41 @@
+package sql
+
+import (
+	"gorm.io/gorm"
+)
+
+type SQL struct {
+	driver           gorm.Dialector
+	activeConnection *gorm.DB
+}
+
+func (db *SQL) Auth() error {
+	// Connect to the database
+	if gormDb, err := gorm.Open(db.driver, &gorm.Config{}); err != nil {
+		return err
+	} else {
+		// Success - store the connection
+		db.activeConnection = gormDb
+	}
+
+	// Perform a simple query to ensure connectivity
+	rows, err := db.activeConnection.Raw("SELECT 1 + 1 AS result").Rows()
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	return nil
+}
+
+func (db *SQL) Close() error {
+	return nil
+}
+
+func (db *SQL) InsertBulk(table string, data []map[string]interface{}) (int, error) {
+	return len(data), nil
+}
+
+func (db *SQL) Truncate(table string) error {
+	return nil
+}
